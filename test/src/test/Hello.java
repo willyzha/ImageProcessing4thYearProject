da0976315693190d9eaedef7d0b4ca7805a6fd6f
Willy Zhang <willyzha@gmail.com>
2016-09-20 22:58:02 -0400

    Add code to read image from webcam and display in GUI

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -3,59 +3,90 @@ package test;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.ImageProducer;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class Hello
 {
-	  public void run() {
-		   Mat image = Highgui.imread("TestImage2.jpg");
-		   CascadeClassifier circleDetector = new CascadeClassifier("cascade.xml");
-		   
-		   MatOfRect detections = new MatOfRect();
-		   circleDetector.detectMultiScale(image, detections);
-		   
-		   System.out.println(String.format("Detected %s faces", detections.toArray().length));
-		   
-		    for (Rect rect : detections.toArray()) {
-		        Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
-		    }
-		    
-		    // Save the visualized detection.
-		    String filename = "faceDetection.png";
-		    System.out.println(String.format("Writing %s", filename));
-		    Highgui.imwrite(filename, image);
-	  }
-	
-	
+	private static final int WEBCAM_PX_HEIGHT = 240; // 720
+	private static final int WEBCAM_PX_WIDTH = 240; // 1280
+	public void run() {
+		Mat image = Highgui.imread("webcamOut.png");
+		CascadeClassifier circleDetector = new CascadeClassifier("cascade.xml");
 
-	  public static void main(String[] args) {
-		  System.out.println("Hello, OpenCV");
-		  // Load the native library.
-		  System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		  new Hello().run();
-	  }
-   
-   
-   public Image toBufferedImage(Mat m){
-       int type = BufferedImage.TYPE_BYTE_GRAY;
-       if ( m.channels() > 1 ) {
-           type = BufferedImage.TYPE_3BYTE_BGR;
-       }
-       int bufferSize = m.channels()*m.cols()*m.rows();
-       byte [] b = new byte[bufferSize];
-       m.get(0,0,b); // get all the pixels
-       BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);
-       final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-       System.arraycopy(b, 0, targetPixels, 0, b.length);  
-       return image;
+		MatOfRect detections = new MatOfRect();
+		circleDetector.detectMultiScale(image, detections);
 
-   }
+		System.out.println(String.format("Detected %s faces", detections.toArray().length));
+
+		for (Rect rect : detections.toArray()) {
+			Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+		}
+
+		// Save the visualized detection.
+		String filename = "faceDetection.png";
+		System.out.println(String.format("Writing %s", filename));
+		Highgui.imwrite(filename, image);
+
+
+	}
+
+	public void webcamCapture() {
+		VideoCapture capture = new VideoCapture(0);
+		capture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, WEBCAM_PX_HEIGHT);
+		capture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, WEBCAM_PX_WIDTH);
+
+		capture.open(0);
+		System.out.println(capture.isOpened());
+
+		Mat image = new Mat();
+		if (capture.read(image)) {
+			Imgproc.resize(image, image, new Size(240, 240));
+			System.out.println("Sucessful");
+			String filename = "webcamOut.png";
+			Highgui.imwrite(filename, image);
+		}	  
+
+		System.out.println("Fail");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, OpenCV");
+		// Load the native library.
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+		Hello test = new Hello();
+
+		test.webcamCapture();
+		test.run();		  
+
+	}
+
+
+	public Image toBufferedImage(Mat m){
+		int type = BufferedImage.TYPE_BYTE_GRAY;
+		if ( m.channels() > 1 ) {
+			type = BufferedImage.TYPE_3BYTE_BGR;
+		}
+		int bufferSize = m.channels()*m.cols()*m.rows();
+		byte [] b = new byte[bufferSize];
+		m.get(0,0,b); // get all the pixels
+		BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(b, 0, targetPixels, 0, b.length);  
+		return image;
+
+	}
 }
 
---
+++ test/src/webcamCaptureGui/WebcamGui.java
@@ -0,0 +1,87 @@
+package webcamCaptureGui;
+
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
+
+public class WebcamGui extends JFrame{
+	private static final int WEBCAM_PX_HEIGHT = 240; // 720
+	private static final int WEBCAM_PX_WIDTH = 240; // 1280
+
+	private JLabel videoFrame;
+	
+	public WebcamGui() {
+		setTitle("Webcam");
+		int width = 1280;
+		int height = 720;
+		setSize(new Dimension(width, height));
+		setVisible(true);
+		
+		setLayout(new GridBagLayout());
+		
+		videoFrame = new JLabel();
+		add(videoFrame);
+	}
+
+	public void webcamCapture() {
+		Thread webcam = new Thread() {
+			public void run() {
+				VideoCapture capture = new VideoCapture(0);
+				capture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, WEBCAM_PX_HEIGHT);
+				capture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, WEBCAM_PX_WIDTH);
+				capture.open(0);
+				
+				Mat image = new Mat();
+				capture.read(image);
+				String filename = "webcamOut.png";
+				Highgui.imwrite(filename, image);
+				
+				while (true && isVisible()) {			
+					if (capture.read(image)) {
+						System.out.println("Sucessful");
+
+					}	  
+			
+					ImageIcon frame = new ImageIcon(bufferedImage(image));
+					System.out.println("START");
+
+					videoFrame.setIcon(frame);
+				    System.out.println("FINISH");
+
+				}
+				capture.release();
+			}
+		};
+		
+		webcam.start();
+	}
+	
+	public static BufferedImage bufferedImage(Mat m) {
+	    int type = BufferedImage.TYPE_BYTE_GRAY;
+	    if ( m.channels() > 1 ) {
+	        type = BufferedImage.TYPE_3BYTE_BGR;
+	    }
+	    BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);
+	    m.get(0,0,((DataBufferByte)image.getRaster().getDataBuffer()).getData()); // get all the pixels
+	    return image;
+	}
+	
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		// TODO Auto-generated method stub
+		WebcamGui gui = new WebcamGui();
+		gui.webcamCapture();
+	}
+
+}
