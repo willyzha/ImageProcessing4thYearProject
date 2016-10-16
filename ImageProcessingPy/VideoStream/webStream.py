a3812b58f6914225caab163cc42ad7cffedb26ca
Willy Zhang <willyzha@gmail.com>
2016-10-16 12:22:06 -0400

    Add Python Code for Image Tracking

---
+++ ImageProcessingPy/VideoStream/webStream.py
@@ -0,0 +1,16 @@
+import web
+
+render = web.template.render('templates/')
+
+urls = ('/', 'index')
+
+class index:
+    def GET(self):
+        name = 'test'
+        return render.index(name)
+    
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+    
