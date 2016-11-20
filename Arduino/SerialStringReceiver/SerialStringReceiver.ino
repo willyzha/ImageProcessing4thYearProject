728a578c0bc950ad651317c01b43bb99f2c7a2b4
willyzha <willyzha@gmail.com>
2016-11-20 14:30:33 -0500

    Working Individual Motor Control

--- Arduino/SerialStringReceiver/SerialStringReceiver.ino
+++ Arduino/SerialStringReceiver/SerialStringReceiver.ino
@@ -2,8 +2,8 @@
 // Open a serial connection and flash LED when input is received
 
 int ledRed = 4;      // LED connected to digital pin 4
-Servo myservo;  // create servo object to control a servo
-Servo yourservo;
+Servo panServo;  // create servo object to control a servo
+Servo tiltServo;
 
 void setup(){
  // Open serial connection.
@@ -11,20 +11,21 @@ void setup(){
  Serial.setTimeout(1000);
  pinMode(ledRed, OUTPUT);     
 
-  myservo.attach(9, 800, 2350);  // attaches the servo on pin 9 to the servo object
-  yourservo.attach(8, 800, 2350);
+  panServo.attach(9, 500, 2350);  // attaches the servo on pin 9 to the servo object
+  tiltServo.attach(8, 500, 2350);
 }
 
 void loop(){
  if(Serial.available() > 0){      // if data present, blink
        String text = Serial.readStringUntil('\n');
 
-       int angle = text.toInt();
-       Serial.println(angle);
+       int panAngle = text.substring(0, text.indexOf(" ")).toInt();
+       int yawAngle = text.substring(text.indexOf(" "), text.indexOf("\n")).toInt();
+       //Serial.println(angle);
 
-       //if (angle < 120) {
-         myservo.write(angle);
-         yourservo.write(angle);
-       //}
+
+         panServo.write(panAngle);
+         tiltServo.write(yawAngle);
+
    }
 }
--- ImageProcessingPy/src/SerialWriter.py
+++ ImageProcessingPy/src/SerialWriter.py
@@ -15,13 +15,13 @@ class SerialPort():
 if __name__ == "__main__":
     port = SerialPort('COM3', 9600, 2)
     angle = 0
-    increment = 1;
+    increment = 10;
     while True:
         print "sending " + str(angle)
-        port.write(str(angle)+"\n")
-        time.sleep(0.01)
+        port.write(str(angle) + " " + str(angle)+"\n")
+        time.sleep(0.1)
         angle = angle + increment
         if angle <= 0:
-            increment = 1
+            increment = 10
         elif angle >= 180:
-            increment = -1
+            increment = -10
