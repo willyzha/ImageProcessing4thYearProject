3c2ecaefd8b01b77e1eea79b9e7bfc5582d460d5
johnhan31 <johnhan31@gmail.com>
2016-11-22 13:23:53 -0500

    testing manual white balance

--- ImageProcessingPy/src/PiCamModule.py
+++ ImageProcessingPy/src/PiCamModule.py
@@ -17,6 +17,7 @@ class PiCam:
         self.frame = None
         self.stopped = False
         self.start()
+        self.calibrateWhiteBalance()
     
     def start(self):
         Thread(target=self.update, args=()).start()
@@ -32,6 +33,39 @@ class PiCam:
                 self.rawCapture.close()
                 self.camera.close()
                 return
+    def calibrateWhiteBalance(self):
+        self.camera.awb_mode = 'off'
+        rg,bg = (0.5, 0.5)
+        self.camera.awb_gains = (rg, bg)
+        with PiRGBArray(self.camera, size=9128, 72)) as output:
+        # Allow 30 attempts to fix AWB
+            for i in range(30):
+                # Capture a tiny resized image in RGB format, and extract the
+                # average R, G, and B values
+                self.camera.capture(output, format='rgb', resize=(128, 72), use_video_port=True)
+                r, g, b = (np.mean(output.array[..., i]) for i in range(3))
+                print('R:%5.2f, B:%5.2f = (%5.2f, %5.2f, %5.2f)' % (
+                    rg, bg, r, g, b))
+                # Adjust R and B relative to G, but only if they're significantly
+                # different (delta +/- 2)
+                if abs(r - g) > 2:
+                    if r > g:
+                        rg -= 0.1
+                    else:
+                        rg += 0.1
+                if abs(b - g) > 1:
+                    if b > g:
+                        bg -= 0.1
+                    else:
+                        bg += 0.1
+                self.camera.awb_gains = (rg, bg)
+                output.seek(0)
+                output.truncate()
+        print 'calibration is completed'
+        if (abs(r - g) > 2) and (abs(b - g) > 1)
+            # whitebalance is failed, manual white balance is aborted and auto awb is set
+            print 'calibration was not successful'
+            self.camera.awb_mode='auto'
 
     def getFrame(self):
         frame = self.frame        
