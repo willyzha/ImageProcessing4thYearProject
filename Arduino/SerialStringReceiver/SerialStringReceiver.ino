a418d7c8687a7417958f138e51531252736db10a
johnhan <johnhan31@gmail.com>
2016-11-26 03:35:12 +0000

    Modify arduino code to not constantly update servo with same value

--- Arduino/SerialStringReceiver/SerialStringReceiver.ino
+++ Arduino/SerialStringReceiver/SerialStringReceiver.ino
@@ -5,11 +5,17 @@ int ledRed = 4;      // LED connected to digital pin 4
 Servo panServo;  // create servo object to control a servo
 Servo tiltServo;
 
+int currPan;
+int currTilt;
+
 void setup(){
  // Open serial connection.
  Serial.begin(9600);
  Serial.setTimeout(1000);
  pinMode(ledRed, OUTPUT);     
+
+  currPan = 90;
+  currTilt = 90;
 
   panServo.attach(9, 550, 2350);  // attaches the servo on pin 9 to the servo object
   tiltServo.attach(8, 550, 2350);
@@ -18,14 +24,16 @@ void setup(){
 void loop(){
  if(Serial.available() > 0){      // if data present, blink
        String text = Serial.readStringUntil('\n');
-
+       
        int panAngle = text.substring(0, text.indexOf(" ")).toInt();
        int yawAngle = text.substring(text.indexOf(" "), text.indexOf("\n")).toInt();
        //Serial.println(angle);
-
-
+       if(currPan != panAngle) {
          panServo.write(panAngle);
-         tiltServo.write(yawAngle);
 
+       }
+       if(currTilt != yawAngle) {
+         tiltServo.write(yawAngle);
+       }
    }
 }
